f7833a4b460e631a9ae9e125bf96b4f802ea3944
Famgithub-server <fam0637186002@gmail.com>
2025-12-23 15:17:08 +0700

    update

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ifstream data("score.txt");
+
+    int i = 0;
+    float sum = 0, sum_sq = 0;
+    string v;
+    while (getline(data, v)) {
+        sum += stof(v);
+        sum_sq += stof(v) * stof(v);
+        i++;
+    }
+    cout << "Number of data = "<<i<<"\n";
+    cout << setprecision(3);
+    float mean = 1.0 * sum/i;
+    cout << "Mean = "<<mean<<"\n";
+    cout << "Standard deviation = "<<sqrt(1.0 * sum_sq/i - mean * mean);
+
+    data.close();
+}
